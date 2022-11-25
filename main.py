06f7b1ab245f9e9fe15cfd94d42986cc1ed9e09f
cleitonleal <cleitoncsl@gmail.com>
2022-11-24 21:12:28 -0300

    v1

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import string
 
 
 # Libs
-
+from tweepy import Client
 from wordcloud import WordCloud, STOPWORDS
 from textblob import TextBlob
 from PIL import Image
@@ -28,7 +28,22 @@ consumerKey = "wWuj4J2HbU4RQqbnXDnQJeSRx"
 consumerSecret = "BJqKI9X06mJrlRCKwWJAxsBHowzibjFzzJ1Dg7sCp6Z6uNEEnE"
 accessToken = "1595773759922790400-1kPiEzo74ui0OHxQMP19AbtWq5D1S0"
 accessTokenSecret = "FLOmS5OnDhGGJdlryHU5Itka1pW98CMfIisbWwVyW2K5Z"
-auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
-auth.set_access_token(accessToken, accessTokenSecret)
-api = tweepy.API(auth)
+acecessTokenBearer = "AAAAAAAAAAAAAAAAAAAAAEnAjgEAAAAAmCrxkKx137BwxTGlk0I0QU2ptHY%3DrTsUa9LIS6VcxAT4A92qDMvZZJhbFzaL3F3UtJg44T0F2B5gSS"
 
+client = tweepy.Client(bearer_token=acecessTokenBearer)
+query = "#apple -is:retweet lang:en"
+paginator = tweepy.Paginator(
+    client.search_recent_tweets,
+    query=query,
+    max_results=100,
+    limit=10
+)
+
+tweet_list = []
+for tweet in paginator.flatten():
+    tweet_list.append(tweet)
+    print(tweet)
+
+tweet_list_df = pd.DataFrame(tweet_list)
+tweet_list_df = pd.DataFrame(tweet_list_df['text'])
+tweet_list_df.head(5)
